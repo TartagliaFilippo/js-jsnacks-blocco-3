68428270fb42aa86b5b0db1d839475ed9b72190a
filippo199600 <pippotartaglia@gmail.com>
2023-07-26 16:44:24 +0200

    snack 2 e 3

--- js/main.js
+++ js/main.js
@@ -1,14 +1,46 @@
-//  chiedo un numero min
-const min = parseInt(
-  prompt("scrivi un numero che sia il tuo intervallo più basso")
-);
+// // ! ESERCIZIO 1
+// //  chiedo un numero min
+// const min = parseInt(
+//   prompt("scrivi un numero che sia il tuo intervallo più basso")
+// );
 
-//  chiedo un secondo numero max
-const max = parseInt(
-  prompt("scrivi un numero che sia il tuo intervallo più alto")
-);
+// //  chiedo un secondo numero max
+// const max = parseInt(
+//   prompt("scrivi un numero che sia il tuo intervallo più alto")
+// );
 
-//  genero un numero casuale e nella formula inserico il mio max e il min
-const number = Math.floor(Math.random() * (max - min)) + min;
+// //  genero un numero casuale e nella formula inserico il mio max e il min
+// const number = Math.floor(Math.random() * (max - min)) + min;
 
-console.log(number);
+// console.log(number);
+
+// // ! ESERCIZIO 2
+// //  chiedo la prima parola
+// const wordSt = prompt("inserisci una parola");
+
+// //  chiedo una seconda parola
+// const wordNd = prompt("inserisci una parola");
+
+// // inserisco le condizioni
+// if (wordSt.length == wordNd.length) {
+//   console.log(wordSt, wordNd);
+// } else if (wordSt.length > wordNd.length) {
+//   console.log(wordSt);
+// } else {
+//   console.log(wordNd);
+// }
+
+// ! ESERCIZIO 3
+// CREO UN ARRAY VUOTO
+const myArr = [];
+
+// CREO UN CONTATORE SOMMA
+let sum = 0;
+
+while (sum < 50) {
+  let numberUser = parseInt(prompt("dammi un numero"));
+  sum += numberUser;
+  myArr.push(sum);
+}
+
+console.log(sum);
--- readme.md
+++ readme.md
@@ -2,8 +2,31 @@
 
 Chiedi all'utente un primo numero, poi un secondo e genera un numero casuale compreso tra i due.
 
-## SVOLGIMENTO
+## SVOLGIMENTO TRACCIA 1
 
 - chiedo un numero min
 - chiedo un secondo numero max
 - genero un numero casuale e nella formula inserico il mio max e il min
+
+# TRACCIA 2
+
+Dare la possibilità di inserire due parole. Verificare che le due parole abbiano la stessa lunghezza. Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.
+
+## SVOLGIMENTO TRACCIA 2
+
+- chiedo la prima parola
+- chiedo una seconda parola
+- stabilisco una costante per la lunghezza delle parole
+- inserico un **IF** che
+  - se lunghe uguali, le stampa entrambi
+  - **ELSE** stampa la più lunga
+
+# TRACCIA 3
+
+Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
+
+## SVOLGIMENTO TRACCIA 3
+
+- creo un array vuoto
+- inserisco un ciclo do while che
+  - chiedo un numero **finchè** non arrivo a 50
