1d64b72f8412ec8484158e62c46c08eba9841311
filippo199600 <pippotartaglia@gmail.com>
2023-07-26 15:17:14 +0200

    first snack

---
+++ js/main.js
@@ -0,0 +1,14 @@
+//  chiedo un numero min
+const min = parseInt(
+  prompt("scrivi un numero che sia il tuo intervallo più basso")
+);
+
+//  chiedo un secondo numero max
+const max = parseInt(
+  prompt("scrivi un numero che sia il tuo intervallo più alto")
+);
+
+//  genero un numero casuale e nella formula inserico il mio max e il min
+const number = Math.round(Math.random() * max) + min;
+
+console.log(number);
---
+++ readme.md
@@ -0,0 +1,9 @@
+# TRACCIA 1
+
+Chiedi all'utente un primo numero, poi un secondo e genera un numero casuale compreso tra i due.
+
+## SVOLGIMENTO
+
+- chiedo un numero min
+- chiedo un secondo numero max
+- genero un numero casuale e nella formula inserico il mio max e il min
